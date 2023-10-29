6d5bb38f9bc6b8ba901db370703287c481e77346
sotirisaristoglou <sotirisaristoglou@gmail.com>
2023-10-29 20:44:35 +0100

    Modify

--- components/genericComponents/Person/Person.js
+++ components/genericComponents/Person/Person.js
@@ -17,19 +17,14 @@ export default class Person extends Component {
 							<h1>Resume {this.props.blok.title} {this.props.blok.lastname} {this.props.blok.firstname}</h1>
 						</div>
 						<div className={[css["box"], css["sidebar"]].join(" ")}>
-							<div className={css["personalimage"]}><img src={this.props.blok.photo.filename} /></div>
+							<div className={css["personalimage"]}><img src={this.props.blok.image.filename} /></div>
 							<div className={css["personaldetails"]}>
 								<div className={css["personaldetailitem"]}>{this.props.blok.title} {this.props.blok.firstname} {this.props.blok.lastname}</div>
 								<div className={css["personaldetailitem"]}>{this.props.blok.dateofbirth}</div>
 								<div className={css["personaldetailitem"]}>{this.props.blok.location}</div>
 							</div>
 						</div>
-						<div className={[css["box"], css["experience"]].join(" ")}>
-							<h2>Experience</h2>
-							{this.props.blok.experiences.map((nestedBlok) => (
-								<StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
-							))}
-						</div>
+
 						<div className={[css["box"], css["foot"]].join(" ")}>
 							<div>&copy; {this.props.blok.firstname} {this.props.blok.lastname} {new Date().getFullYear()}</div>
 						</div>
